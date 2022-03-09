e1816ec6661ad9bd988ba2e8c6989a7a0b2b4c33
Jannis Blossey <57671651+jblossey@users.noreply.github.com>
2022-03-09 12:32:48 +0100

    refactor(naming): renamed CopyrightProtectionCollection to LicenseAgreementCollection

--- contracts/lawCompliersAgreement.sol
+++ contracts/lawCompliersAgreement.sol
@@ -22,13 +22,13 @@ contract LawCompliersAgreement is Initializable {
     /**
      * @dev data structure that stores all license agreements to creators' digital goods with multiple subscribers
      */
-    struct DigitalCopyrightProtectionCollection {
+    struct LicenseAgreementCollection {
         string creatorId;
         string license;
         Subscription[] subscriptions;
     }
 
-    mapping (uint => DigitalCopyrightProtectionCollection) copyrightRegistrations;
+    mapping (uint => LicenseAgreementCollection) copyrightRegistrations;
 
     function initialize() public initializer {}
 
@@ -38,9 +38,9 @@ contract LawCompliersAgreement is Initializable {
      * @param creatorId the unique identifier that connects an agreement to its originator
      * @param license the license under which the creator publishes the digital good
      */
-    function registerDigitalGood(uint watermark, string calldata creatorId, string calldata license) external returns (DigitalCopyrightProtectionCollection memory) {
+    function registerDigitalGood(uint watermark, string calldata creatorId, string calldata license) external returns (LicenseAgreementCollection memory) {
         require(bytes(copyrightRegistrations[watermark].creatorId).length == 0, "Encountered a hash collision. Aborting operation.");
-        DigitalCopyrightProtectionCollection storage dcpc = copyrightRegistrations[watermark];
+        LicenseAgreementCollection storage dcpc = copyrightRegistrations[watermark];
         dcpc.creatorId = creatorId;
         dcpc.license = license;
         return dcpc;
@@ -50,7 +50,7 @@ contract LawCompliersAgreement is Initializable {
      * @dev return the digital license agreement of a watermark
      * @param watermark the unique identifier of a license agreement
      */
-    function readAgreement(uint watermark) external view returns (DigitalCopyrightProtectionCollection memory dcpc) {
+    function readAgreement(uint watermark) external view returns (LicenseAgreementCollection memory dcpc) {
         // check if digital good even exists
         require(bytes(copyrightRegistrations[watermark].creatorId).length != 0, "The digital good you are trying to subscribe to was not yet registered. Make sure the watermark is calculated correctly or contact the creator.");
         return copyrightRegistrations[watermark];
@@ -70,7 +70,7 @@ contract LawCompliersAgreement is Initializable {
      * @dev Saves and publicly announces aggreement to license terms of digital good
      * @param subscriberId unique identifier of subscriber for off-chain accountability
      */
-    function agreeToTermsAndSubscribe(uint watermark, string calldata subscriberId) external returns (DigitalCopyrightProtectionCollection memory, Subscription memory) {
+    function agreeToTermsAndSubscribe(uint watermark, string calldata subscriberId) external returns (LicenseAgreementCollection memory, Subscription memory) {
         // check if digital good even exists
         require(bytes(copyrightRegistrations[watermark].creatorId).length != 0, "The digital good you are trying to subscribe to was not yet registered. Make sure the watermark is calculated correctly or contact the creator.");
         // add subscription to contract storage
